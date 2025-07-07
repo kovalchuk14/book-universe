df9e608e71b3995b2f2739d388f4809f1594870a
vladyslav <vladyslav.korniienko@stud.onu.edu.ua>
2025-07-07 22:37:40 +0300

    added feedbacks-section for link feedback js files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-import { renderFeedbacks } from "/js/feedback-dom.js";
-import { initFeedbackSwiper } from "/js/feedback-swiper.js";
-
-// Відгуки в DOM
-renderFeedbacks();
-
-// Ініціалізація Swiper після того, як DOM побудовано
-initFeedbackSwiper();
+import "/js/feedbacks-section.js";
